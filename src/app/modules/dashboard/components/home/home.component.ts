6bf37a5bf00b1f0b8eb9dbc625d087215fddfbd0
JoelTejerina <catrieltejerina@gmail.com>
2022-09-07 00:30:29 -0300

    Graficos Productos

--- src/app/modules/dashboard/components/home/home.component.ts
+++ src/app/modules/dashboard/components/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Chart } from 'chart.js';
+import { ProductoElement } from 'src/app/modules/producto/producto/producto.component';
+import { ProductoService } from 'src/app/modules/shared/services/producto.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +10,60 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  chartBar:any;
+  chartDoughnut:any;
+
+  constructor(private  productoService: ProductoService) { }
 
   ngOnInit(): void {
+    this.getProductos();
   }
 
+  getProductos(){
+    this.productoService.getProductos()
+        .subscribe( (data:any) => {
+          console.log("respuesta productos: ", data);
+          this.procesarProductosResponse(data);
+        }, (error: any) => {
+          console.log("error: ", error);
+        })
+  }
+
+  procesarProductosResponse(resp: any){
+
+    const nombreProducto: String [] = [];
+    const cantidad: number [] = [];
+
+
+        if( resp.metadata[0].codigo == "200") {
+          let listProducto = resp.productoResponse.productos;
+
+          listProducto.forEach((element: ProductoElement) => {
+         
+          nombreProducto.push(element.nombre);
+          cantidad.push(element.cantidad);
+       });
+
+       this.chartBar = new Chart('canvas-bar', {
+         type: 'bar',
+         data: {
+           labels: nombreProducto,
+           datasets: [
+             { label: 'Productos', data: cantidad}
+           ]
+         }
+       });
+
+       this.chartDoughnut = new Chart('canvas-doughnut', {
+        type: 'doughnut',
+        data: {
+          labels: nombreProducto,
+          datasets: [
+            { label: 'Productos', data: cantidad}
+          ]
+        }
+       });
+      }
+      
+  }
 }
--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -5,7 +5,9 @@ import { HomeComponent } from './components/home/home.component';
 import { SharedModule } from '../shared/shared.module';
 import { CategoriModule } from '../categoria/categoria.module';
 import { ProductoModule } from '../producto/producto.module';
-
+import { MaterialModule } from '../shared/material.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { NgChartsModule } from 'ng2-charts';
 
 
 @NgModule({
@@ -16,7 +18,10 @@ import { ProductoModule } from '../producto/producto.module';
     CommonModule,
     SharedModule,
     CategoriModule,
-    ProductoModule
+    ProductoModule,
+    MaterialModule,
+    FlexLayoutModule,
+    NgChartsModule
   ]
 })
 export class DashboardModule { }
